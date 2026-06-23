0be676f1f79256ba9d96ccba676aec1f4d109ff5
chensir91 <3355911900@qq.com>
2026-06-23 18:13:35 +0800

    v4.1: clean rebuild from v4.0 + live preview screen

--- app/src/main/java/com/ihatethis/FloatingTextService.java
+++ app/src/main/java/com/ihatethis/FloatingTextService.java
@@ -2,7 +2,6 @@ package com.ihatethis;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Color;
